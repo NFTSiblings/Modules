fb685087147bde9625d629fe3f5f9b46605dcf35
ethan-sibs <102136771+ethan-sibs@users.noreply.github.com>
2022-06-02 14:56:16 +0900

    Updated imports from npm to direct local import

--- BurnExternal_V1.sol
+++ BurnExternal_V1.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@siblings/modules/AdminPrivileges.sol";
+import "./AdminPrivileges.sol";
 
 interface BurnInterface {
     function burn(uint256 tokenId) external;
--- ERC20Payment.sol
+++ ERC20Payment.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@siblings/modules/AdminPrivileges.sol";
+import "./AdminPrivileges.sol";
 
 interface IERC20 {
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
