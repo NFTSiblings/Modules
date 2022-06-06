d6c5764341a7c1d6d8ea94da19701a291a0d218d
ethan-sibs <102136771+ethan-sibs@users.noreply.github.com>
2022-06-06 18:29:23 +0900

    Added MerkleProofAllowlist.sol

---
+++ MerkleProofAllowlist.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+/**
+* @notice THIS PRODUCT IS IN BETA, SIBLING LABS IS NOT RESPONSIBLE FOR ANY LOST FUNDS OR
+* UNINTENDED CONSEQUENCES CAUSED BY THE USE OF THIS PRODUCT IN ANY FORM.
+*/
+
+/**
+ * @dev Contract module which provides allowlist functionality
+ * using the cryptographic technique called Merkle Proof.
+ *
+ * Inheriting from `MerkleProofAllowlist` will make the
+ * {requireMerkleProof} modifier available - add this modifier
+ * to a function in your contract to require that the
+ * caller provide a valid merkle proof, and that their
+ * address is on the allowlist.
+ *
+ * Merkle proofs must be provided as a bytes32 array.
+ *
+ * Provide the Merkle root to the constructor, or update
+ * it with the {setMerkleRoot} function.
+ *
+ * See more module contracts from Sibling Labs at
+ * https://github.com/NFTSiblings/Modules
+ */
+contract MerkleProofAllowlist is AdminPrivileges {
+    bytes32 merkleRoot;
+
+    constructor(bytes32 _merkleRoot) {
+        merkleRoot = _merkleRoot;
+    }
+
+    function setMerkleRoot(bytes32 root) public onlyAdmins {
+        merkleRoot = root;
+    }
+
+    modifier requireMerkleProof(bytes32[] calldata _merkleProof) {
+        bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+        require(
+            MerkleProof.verify(_merkleProof, merkleRoot, leaf),
+            "MerkleProofAllowlist: Invalid Merkle Proof"
+        );
+        _;
+    }
+}
